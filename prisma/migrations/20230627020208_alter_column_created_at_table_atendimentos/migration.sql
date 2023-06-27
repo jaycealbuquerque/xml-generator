5486a471320c3f6a3398f3197a2e1b1db846e326
“jaycealbuquerque” <jeycealbuquerquerocha@gmail.com>
2023-06-26 23:29:01 -0300

    feat: find by data_atendiment and order by numero_atendimento

---
+++ prisma/migrations/20230627020208_alter_column_created_at_table_atendimentos/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "atendimentos" ALTER COLUMN "created_at" DROP DEFAULT;
--- src/use-cases/create-atendimento.ts
+++ src/use-cases/create-atendimento.ts
@@ -1,28 +1,43 @@
+import dayjs from 'dayjs'
 import { prisma } from '../lib/prisma'
 import { ServiceNumberGenerator } from '../provider/ service-number-generator'
 
 export class CreateAtendimentoUseCase {
-  async execute(dataAtendimento) {
+  async execute({ dataAtendimento }) {
     const atendimentoExist = await prisma.atendimentos.findFirst({
-      where: { created_at: 2023-06 - 24 },
+      orderBy: { numero_atendimento: 'desc' },
+      where: { data_atendimento: { equals: dataAtendimento } },
     })
+
+    // const hoje = dayjs().format('2023-06-24')
     // const createAtendimento = await prisma.atendimentos.create({
-    //   data: { numero_atendimento: 20230623001005 },
+    //   data: {
+    //     numero_atendimento: 20230624001003,
+    //     data_atendimento: hoje,
+    //   },
     // })
 
     // const { numero_atendimento } = await prisma.atendimentos.findFirst({
     //   orderBy: { numero_atendimento: 'asc' },
     // })
+    // console.log(atendimentoExist?.numero_atendimento.toString())
 
     // const gerador = new ServiceNumberGenerator()
-    // const numero = numero_atendimento.toString()
+    // const numero = atendimentoExist?.numero_atendimento.toString()
     // const numeroGerado = gerador.gerarNumeroAtendimento(numero)
     // const novoGerado = gerador.gerarNumeroAtendimento(numeroGerado)
     // console.log(numeroGerado, novoGerado)
+
     // const dataInformada = dayjs('2023-04-11')
     // const antendimentoInformado =
     //   gerador.gerarNumeroAtendimentoComData(dataInformada)
     // console.log(antendimentoInformado)
+    // console.log(atendimentoExist)
+
+    BigInt.prototype.toJSON = function () {
+      return this.toString()
+    }
+
     return atendimentoExist
   }
 }
